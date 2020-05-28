5db17ce525f72275596af65d008bcc5d5473ff86
Fabián Sánchez L <fadarsaleeing@gmail.com>
2020-05-28 17:23:45 -0500

    build

--- src/server.js
+++ src/server.js
@@ -8,4 +8,4 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/dominancia/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
